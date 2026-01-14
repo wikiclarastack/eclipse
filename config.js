4e4cf95292b2ea995c93c9a2f91abc03385f44e0
wikiclarastack <wikiclarastack@gmail.com>
2026-01-14 19:31:27 -0300

    Update and rename config,js to config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+const SYSTEM_CONFIG = {
+    ADMIN_EMAIL: "guizinbzsk@gmail.com",
+    
+    // WEBHOOK DE SEGURANÇA
+    SECURITY_WEBHOOK: "https://discord.com/api/webhooks/1461120663317385399/BYvrq6F6QPwNenCbsHdWL7ML-HryAnH209fIvWJg1tGpRNX-SJOULfjfPAJrobWqCzze",
+    
+    // CONFIGURAÇÕES DO FIREBASE
+    FIREBASE_CONFIG: {
+        apiKey: "AIzaSyCeAkDlLb3cAnYQvpo2BwHSB8qktUq7Zbg",
+        authDomain: "eclipsexploits.firebaseapp.com",
+        databaseURL: "https://eclipsexploits-default-rtdb.firebaseio.com",
+        projectId: "eclipsexploits",
+        storageBucket: "eclipsexploits.firebasestorage.app",
+        messagingSenderId: "126850895726",
+        appId: "1:126850895726:web:94e544d9f2d373671abb7f",
+        measurementId: "G-LH6C7CXH10"
+    },
+    
+    // LIMITES E CONFIGURAÇÕES
+    CHAT_COOLDOWN: 3000,
+    MAX_MESSAGES: 100,
+    MAX_SCRIPTS: 50
+};
